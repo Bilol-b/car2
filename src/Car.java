4a4f2e57a9fc9093bd6a8a1d94e09c5e634155f2
Bilol <Bilol@gmail.ru>
2024-10-07 17:41:11 +0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,52 @@
+public class Car {
+
+    public String getColour(String model) {
+        return model;
+    }
+
+    public String getModel(String colour) {
+        return colour;
+    }
+
+    public String getUzatma(String uzatma) {
+        return uzatma;
+    }
+
+    public String getNarx(String narx) {
+        return narx;
+    }
+
+    public class Uchburchak {
+
+        public String katetA(String kateta) {
+            return kateta;
+        }
+
+        public String katetB(String katetb) {
+            return katetb;
+        }
+
+        public String gipotenuzcC(String gipotenuzaC) {
+            return gipotenuzaC;
+        }
+    }
+
+    public class Percon {
+
+        public String getJinsi(String jins) {
+            return jins;
+        }
+
+        public String getYoshi(String yosh) {
+            return yosh;
+        }
+
+        public String getBoyi(String boyi) {
+            return boyi;
+        }
+
+        public String getVazni(String vazni) {
+            return vazni;
+        }
+    }
+}
